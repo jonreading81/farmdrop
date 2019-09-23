900480c0586523ab78da71e7aaa0df6ec6d84183
Jonathan Reading <jon.reading.email@gmail.com>
2019-09-23 09:42:49 +0100

    Added fixed layout for header

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
-import { green_1, white} from '../../constants/colors';
-
+import { green_1, white, grey_2} from '../../constants/colors';
 import { StyledContentWrapper } from '../../styles/layout';
 
 export const StyleHeader = styled.header`
+  position:fixed;
+  z-index: 1;
+  top:0;
+  background: ${white};
+  width:100%;
+  border-bottom: 1px solid ${grey_2};
 `;
 
 export const StyledMasthead = styled.div`
@@ -12,13 +17,13 @@ export const StyledMasthead = styled.div`
 `;
 
 export const StyledBasketWrapper = styled.div`
+  margin-top:10px;
   height: 30px;
 `;
 
 export const StyledBasketContentWrapper = styled(StyledContentWrapper)`
   display:flex;
   justify-content: flex-end;
-  margin-top: 0.5rem;
 `;
 
 export const StyledIconWrapper = styled.div`
@@ -28,7 +33,7 @@ export const StyledIconWrapper = styled.div`
 export const StyledBasketCount = styled.div`
     position: absolute;
     right: 0;
-    top: 6px;
+    top: 7px;
     color: ${white};
     font-size: 10px;
     width: 20px;
--- src/containers/app/styles.ts
+++ src/containers/app/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { StyledContentWrapper } from '../../styles/layout';
 
 export const StyledAppContentWrapper = styled(StyledContentWrapper)`
+  margin-top: 120px
   margin-bottom: 2rem;
+
 `;
 
